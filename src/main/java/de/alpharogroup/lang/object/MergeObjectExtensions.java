3afce622b1cbce95b6e6b713baa40cf8ccdafda2
Tatjana Kopalova <tatjana19@users.noreply.github.com>
2016-08-03 09:21:50 +0200

    Create MergeObjectExtensions.java

---
+++ src/main/java/de/alpharogroup/lang/object/MergeObjectExtensions.java
@@ -0,0 +1,9 @@
+package de.alpharogroup.lang.object;
+
+/**
+ * The class {@link MergeObjectExtensions} provide extension methods for merge a source object with another
+ *  object.
+ */
+public final class MergeObjectExtensions
+{
+}

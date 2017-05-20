467e87965dc8407925b1bf76e3fe13f2abd96b2b
Asterios Raptis <asterios.raptis@gmx.net>
2017-05-20 11:25:30 +0200

    Moved bean class to other package.

--- src/main/java/de/alpharogroup/lang/thread/ThreadExtensions.java
+++ src/main/java/de/alpharogroup/lang/thread/ThreadExtensions.java
@@ -1,5 +1,7 @@
 package de.alpharogroup.lang.thread;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import lombok.experimental.UtilityClass;
@@ -17,9 +19,29 @@ public class ThreadExtensions
 	 *
 	 * @return An array with all threads the are currently running.
 	 */
-	public static Thread[] resolveRunningThreads() {
+	public static Thread[] resolveRunningThreads()
+	{
 		final Set<Thread> threadSet = Thread.getAllStackTraces().keySet();
 		final Thread[] threadArray = threadSet.toArray(new Thread[threadSet.size()]);
 		return threadArray;
+	}
+
+
+	/**
+	 * Finds all threads the are currently running and converts them to {@link ThreadDataBean} and
+	 * puts them to a {@link List} that is returned.
+	 *
+	 * @return the new {@link List}
+	 */
+	public static List<ThreadDataBean> newThreadData()
+	{
+		final Thread[] thread = resolveRunningThreads();
+		final List<ThreadDataBean> snapshotOfThreadDataBeans = new ArrayList<>(thread.length);
+		for (int i = 0; i < thread.length; i++)
+		{
+			final Thread t = thread[i];
+			snapshotOfThreadDataBeans.add(ThreadDataBean.of(t));
+		}
+		return snapshotOfThreadDataBeans;
 	}
 }
---
+++ src/main/java/de/alpharogroup/lang/thread/ThreadFieldDataBean.java
@@ -0,0 +1,8 @@
+package de.alpharogroup.lang.thread;
+
+import java.util.Map;
+
+public class ThreadFieldDataBean
+{
+	private Map<String, Object> fields;
+}

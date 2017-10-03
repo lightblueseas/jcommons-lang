bb4d8278188e669a0440cf37caa56d8e935d09f1
Asterios Raptis <asterios.raptis@gmx.net>
2017-10-03 18:08:18 +0200

    created new unit test class

---
+++ src/test/java/de/alpharogroup/file/FilenameExtensionsTest.java
@@ -0,0 +1,14 @@
+package de.alpharogroup.file;
+
+import org.testng.annotations.Test;
+
+public class FilenameExtensionsTest
+{
+
+	@Test
+	public void testGetFilenameWithoutExtensionFile()
+	{
+		// TODO
+	}
+
+}

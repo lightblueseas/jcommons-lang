f9016e8d848cfa6c53a4a43e60b41b3acdcb901a
Asterios Raptis <asterios.raptis@gmx.net>
2018-05-22 18:18:50 +0200

    unit tests extended

---
+++ src/test/java/de/alpharogroup/file/filter/TxtFileFilterTest.java
@@ -0,0 +1,91 @@
+package de.alpharogroup.file.filter;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
+
+import de.alpharogroup.lang.ClassExtensions;
+import de.alpharogroup.test.objects.evaluations.ToStringEvaluator;
+
+/**
+ * The unit test class for the class {@link TxtFileFilter}.
+ */
+public class TxtFileFilterTest
+{
+
+	/**
+	 * Test method for {@link TxtFileFilter#accept(File)}
+	 * 
+	 * @throws URISyntaxException
+	 *             occurs by creation of the file with an uri.
+	 * @throws IOException
+	 *             Signals that an I/O exception has occurred.
+	 */
+	@Test
+	public final void testAccept() throws URISyntaxException, IOException 
+	{
+		boolean expected;
+		boolean actual;
+		FileFilter fileFilter;
+		String filename;
+		String filepath;
+		File file;
+		File dir;
+
+		fileFilter = new TxtFileFilter();
+		assertNotNull(fileFilter);
+
+		filename = "resources.properties";
+
+		filepath = "de/alpharogroup/lang/" + filename;
+
+		file = ClassExtensions.getResourceAsFile(filepath);
+		dir = file.getParentFile();
+
+		actual = fileFilter.accept(file);
+		expected = false;
+		assertEquals(expected, actual);
+
+		actual = fileFilter.accept(dir);
+		expected = true;
+		assertEquals(expected, actual);
+
+		file = new File(".", "TestFind.txt");
+		FileUtils.writeStringToFile(file, "", Charset.defaultCharset());
+
+		actual = fileFilter.accept(file);
+		expected = true;
+		assertEquals(expected, actual);
+		try
+		{
+			file.deleteOnExit();
+		}
+		catch (final Exception e)
+		{
+			// ignore...
+		}
+	}
+
+	/**
+	 * Test method for {@link TxtFileFilter#toString()}
+	 */
+	@Test
+	public final void testToString() 
+	{
+		boolean expected;
+		boolean actual;
+
+		actual = ToStringEvaluator.evaluateConsistency(new TxtFileFilter());
+		expected = true;
+		assertEquals(expected, actual);
+	}
+
+}
